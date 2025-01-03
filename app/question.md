176a1c3c870f1e3a08d9251da01f5db969cd2b4b
qimiaosong <2686130589@qq.com>
2025-01-04 02:22:11 +0800

    delete some other files

--- app/question.md
+++
@@ -1,2 +0,0 @@
-1.sqlalchemy 是什么？和 Orm 的关系 2.为什么执行 sql 语句总有一个 cursor
-3.ORM 中的 session 是什么？有什么作用？
--- app/test.py
+++
@@ -1,5 +0,0 @@
-buffer = [[1, 2, 3, 5, 8]] * 5
-print(buffer[2])
-buffer[1][3] = 10
-
-print(buffer)
